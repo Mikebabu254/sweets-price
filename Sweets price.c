cee18cb7d1fb3403deac47085551fd9ac2ca8313
Mikebabu254 <105848178+Mikebabu254@users.noreply.github.com>
2022-06-20 00:53:36 +0300

    Update Sweets price.c

--- Sweets price.c
+++ Sweets price.c
@@ -4,13 +4,22 @@ A C program that calculates the price of sweets and gives out the price*/
 #include<studio.h>
 main()
 {
-int a;
+int a,b;
 printf("how many sweets do you want\n");
 scanf("%d",&a);
+b=a*10;
 if(a<==-1)
 {
 printf("invalid input");
 }
 else if(a==0)
 printf("it seems you don't like sweets\nwe have new sweet sweets you can try them at affordable price\n);
+}
+else if(a>==1)
+{
+printf("give the cashier %d",b);
+}
+else if(a>==30)
+{
+printf("we don't sell that much sweet in retail\ncan you access for wholesale price\n1:yes\n2:no");
 }
