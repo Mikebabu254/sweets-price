c59ffb4178a87cbef648f2ff3092279509dc97e9
Mikebabu254 <105848178+Mikebabu254@users.noreply.github.com>
2022-06-20 00:35:58 +0300

    Update and rename README.md to Sweets price.c

--- README.md
+++
@@ -1,9 +0,0 @@
-# sweets-price
-/*Just for fun....a c program
-A C program that calculates the price of sweets and gives out the price*/
-#include<studio.h>
-main()
-{
-int a;
-printf("how many sweets do you want");
-}
---
+++ Sweets price.c
@@ -0,0 +1,16 @@
+# sweets-price
+/*Just for fun....a c program
+A C program that calculates the price of sweets and gives out the price*/
+#include<studio.h>
+main()
+{
+int a;
+printf("how many sweets do you want\n");
+scanf("%d",&a);
+if(a<==-1)
+{
+printf("invalid input");
+}
+else if(a==0)
+printf("it seems you don't like sweets\nwe have new sweet sweets you can try them at affordable price\n);
+}
